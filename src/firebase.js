32dfeb783ac2cd5e8761bebbc542d5f5b75206a0
SMhanna23 <saadallah.mhanna23@gmail.com>
2026-03-05 00:25:43 +0200

    add env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,20 +3,13 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  
-  apiKey: "AIzaSyAFvR5OKhAlxfVkkZyb42TryKivtua6EsE",
-
-  authDomain: "lumivite-caa28.firebaseapp.com",
-
-  projectId: "lumivite-caa28",
-
-  storageBucket: "lumivite-caa28.firebasestorage.app",
-
-  messagingSenderId: "237520857766",
-
-  appId: "1:237520857766:web:106610975b8efb2bf7a267"
-
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
